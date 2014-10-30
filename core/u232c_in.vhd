77007ca874cd0d34f25e855899f383593ff0a57a
Yuito Murase <yuito.murase@gmail.com>
2014-10-30 18:24:02 +0900

    update rs232c modules

--- core/u232c_in.vhd
+++ core/u232c_in.vhd
@@ -2,69 +2,137 @@ library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
+package zebius_u232c_in_p is
+
+  type u232c_in_in_t is record
+    take : std_logic;
+  end record;
+
+  type u232c_in_out_t is record
+    ready : std_logic;
+    data : unsigned(7 downto 0);
+  end record;
+
+  component u232c_in is
+      generic (
+        wtime: unsigned(15 downto 0) := x"1ADB");
+      port (
+        clk  : in  std_logic;
+        rx   : in  std_logic;
+        din  : in  u232c_in_in_t;
+        dout : out u232c_in_out_t);
+  end component;
+
+end package;
+
+
+
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+library work;
+use work.zebius_u232c_in_p.all;
+
+
 entity u232c_in is
-  generic (wtime: std_logic_vector(15 downto 0) := x"1ADB");
-  port (clk   : in  std_logic;
-        go    : out std_logic := '0';
-        data  : out std_logic_vector (7 downto 0);
-        rx    : in  std_logic);
+  generic (
+    wtime: unsigned(15 downto 0) := x"1ADB");
+  port (
+    clk   : in  std_logic;
+    rx   : in  std_logic;
+    din  : in  u232c_in_in_t;
+    dout : out u232c_in_out_t);
 end u232c_in;
 
-architecture blackbox of u232c_in is
-  signal buf       : std_logic_vector(7 downto 0)  := (others=>'1');
-  signal countdown : unsigned(15 downto 0) := (others=>'0');
 
-  type   state is (waiting, init, reading, done);
-  signal st        : state := waiting;
-  signal idx       : unsigned(2 downto 0) := "000";
-  signal debug_st  : std_logic_vector(1 downto 0);
+architecture blackbox of u232c_in is
+
+  type state is (waiting, reading, done);
+
+  type ratch_t is record
+    -- interface
+    ready : std_logic;
+    buf : unsigned(7 downto 0);
+    -- receive
+    recvbuf : unsigned(7 downto 0);
+    countdown : unsigned(15 downto 0);
+    st : state;
+    idx : integer range -1 to 7;
+  end record;
+
+  signal r, rin : ratch_t := (
+    ready => '0',
+    buf => (others => '-'),
+    recvbuf => (others => '0'),
+    countdown => wtime,
+    st => waiting,
+    idx => 0);
+
 begin
-  statemachine: process(clk)
+
+  combinational: process(r, rx, din)
+    variable v : ratch_t;
   begin
-    if rising_edge(clk) then
-      case st is
-        when waiting =>
-          go <= '0';            
-          if rx='0' and countdown = 0 then
-            st  <= init;
-            countdown <= shift_right(unsigned(wtime), 3);
-          elsif countdown /= 0 then
-            countdown <= countdown-1;
-          end if;
-        when init =>
-          if countdown = 0 then
-            if rx = '0' then
-              st <= reading;
-              idx <= "000";
-              countdown <= unsigned(wtime);
-            else
-              st <= waiting;
-            end if;
-          elsif countdown /= 0 then
-            countdown <= countdown-1;
-          end if;
-        when reading =>
-          if countdown = 0 then
-            buf <= rx & buf(7 downto 1);
-            countdown <= unsigned(wtime);
-            if idx /= 7 then
-              idx <= idx+1;
-            else
-              st <= done;
-            end if;
-          else
-            countdown <= countdown-1;
-          end if;
-        when done =>
-          data <= buf;
-          go   <= '1';
-          st <= waiting;
-      end case;
+    v := r;
+
+    case v.st is
+      when WAITING =>
+        if rx = '0' then
+          v.st := READING;
+          v.countdown := shift_right(unsigned(wtime), 1);
+          v.idx := -1;
+        end if;
+      when others => null;
+    end case;
+
+    if din.take = '1' then
+      v.ready := '0';
     end if;
+
+    dout.ready <= v.ready;
+
+    rin <= v;
   end process;
 
-  debug_st <= "00" when st = waiting else
-              "01" when st = init else
-              "10" when st = reading else
-              "11" when st = done;
+  sequential: process(clk)
+    variable v : ratch_t;
+  begin
+    if rising_edge(clk) then
+      v := rin;
+
+      case v.st is
+        when READING =>
+          if v.countdown = 0 then
+            v.recvbuf := rx & v.recvbuf(7 downto 1);
+            v.countdown := wtime;
+
+            if v.idx /= 7 then
+              v.idx := v.idx+1;
+            else
+              assert v.ready = '0' report "buffered byte is not still read. This byte will be abondoned.";
+
+              v.st := DONE;
+              v.buf := v.recvbuf;
+              v.ready := '1';
+
+              dout.data <= v.buf;
+            end if;
+          else
+            v.countdown := v.countdown-1;
+          end if;
+
+        when DONE =>
+          if v.countdown = 0 then
+            v.st := WAITING;
+          else
+            v.countdown := v.countdown-1;
+          end if;
+
+        when others => null;
+      end case;
+
+      r <= v;
+    end if;
+  end process;
 end blackbox;
--- core/u232c_out.vhd
+++ core/u232c_out.vhd
@@ -5,7 +5,7 @@ use ieee.numeric_std.all;
 package zebius_u232c_out_p is
 
   type u232c_out_in_t is record
-    data : std_logic_vector (7 downto 0);
+    data : unsigned (7 downto 0);
     go   : std_logic;
   end record;
 
@@ -19,10 +19,10 @@ package zebius_u232c_out_p is
       wtime : unsigned(15 downto 0));
     port (
       clk  : in  std_logic;
-      data : in  std_logic_vector (7 downto 0);
-      go   : in  std_logic;
-      busy : out std_logic;
-      tx   : out std_logic);
+      tx   : out std_logic;
+
+      din  : in  u232c_out_in_t;
+      dout : out u232c_out_out_t);
   end component;
 
 end zebius_u232c_out_p;
@@ -36,6 +36,9 @@ library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
+library work;
+use work.zebius_u232c_out_p.all;
+
 
 entity u232c_out is
   generic (
@@ -43,47 +46,94 @@ entity u232c_out is
     wtime: unsigned(15 downto 0) := x"1ADB");
   port (
     clk  : in  std_logic;
-    data : in  std_logic_vector (7 downto 0);
-    go   : in  std_logic;
-    busy : out std_logic;
-    tx   : out std_logic);
+    tx   : out std_logic;
+
+    din  : in  u232c_out_in_t;
+    dout : out u232c_out_out_t);
 end u232c_out;
 
 
 architecture blackbox of u232c_out is
-  signal countdown : unsigned(15 downto 0) := (others=>'0');
-  signal sendbuf : unsigned(8 downto 0) := (others=>'1');
-  signal state : unsigned(3 downto 0) := "1111";
+
+  type ratch_t is record
+    -- buf
+    buf : unsigned(7 downto 0);
+    busy : std_logic;
+    -- send
+    countdown : unsigned(15 downto 0);
+    sendbuf : unsigned(8 downto 0);
+    state : unsigned(3 downto 0);
+  end record;
+
+  signal r, rin : ratch_t := (
+    buf => (others => '1'),
+    busy => '0',
+    countdown => (others => '0'),
+    sendbuf => (others => '1'),
+    state => "1111");
 
   file ofile : text is out "output.txt";
 begin
-  statemachine: process(clk)
+
+  cobinational: process(r, din)
+    variable v: ratch_t;
     variable l : line;
   begin
-    if rising_edge(clk) then
-      case state is
-        when "1011"=>
-          if go='1' then
-            if report_write then
-              write(l, to_integer(unsigned(data)));
-              writeline(ofile, l);
-            end if;
+    v := r;
 
-            sendbuf<=unsigned(data)&"0";
-            state<=state-1;
-            countdown<=wtime;
-          end if;
-        when others=>
-          if countdown=0 then
-            sendbuf<="1"&sendbuf(8 downto 1);
-            countdown<=wtime;
-            state<=state-1;
+    if din.go = '1' and v.busy = '0' then
+      if report_write then
+        write(l, to_integer(unsigned(din.data)));
+        writeline(ofile, l);
+      end if;
+
+      v.buf := din.data;
+      v.busy := '1';
+    end if;
+
+    dout.busy <= v.busy;
+
+    rin <= v;
+  end process;
+
+
+  statemachine: process(clk)
+    variable v : ratch_t;
+  begin
+    if rising_edge(clk) then
+      v := rin;
+
+      case v.state is
+        when "1001" =>
+          if v.countdown = 0 then
+            if v.busy = '1' then
+              v.sendbuf := v.buf & "0";
+              v.state := v.state-1;
+              v.countdown := wtime;
+              v.busy := '0';
+            end if;
           else
-            countdown<=countdown-1;
+            v.countdown := v.countdown-1;
+          end if;
+
+        when others=>
+          if v.countdown = 0 then
+            v.sendbuf := "1" & v.sendbuf(8 downto 1);
+            v.countdown := wtime;
+
+            if v.state = "0000" then
+              v.state := "1001";
+            else
+              v.state := v.state-1;
+            end if;
+          else
+            v.countdown := v.countdown-1;
           end if;
       end case;
+
+      tx <= v.sendbuf(0);
+      r <= v;
     end if;
   end process;
-  tx<=sendbuf(0);
-  busy<= '0' when state="1011" else '1';
+
 end blackbox;
