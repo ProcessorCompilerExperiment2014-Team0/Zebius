64f122188fe07506770439b320cf987981439de2
minus3theta <yone_j_synthesis@yahoo.co.jp>
2014-11-06 19:05:50 +0900

    asm: implement offset option

--- asm/zasm.ml
+++ asm/zasm.ml
@@ -3,14 +3,35 @@ open AsmParser
 open AsmLexer
 open Conv
 
-;;
+exception Option_format
+
+type opt = {mutable vhdl: bool; mutable offset: int}
 
 let write ot tbl asm =
   List.iter (output_byte ot) (List.concat (List.map (Conv.encode tbl) asm))
 
+let print_options () =
+  prerr_endline "Option format error";
+  prerr_endline "Usage: zasm [input file] [options]";
+  prerr_endline "  -v     Output for vhdl";
+  prerr_endline "  -s <n> Set offset as n words"
+
+let rec get_option n opt =
+  if n < Array.length Sys.argv
+  then match Sys.argv.(n) with
+  | "-v" -> opt.vhdl <- true;
+    get_option (n+1) opt
+  | "-s" ->
+    if (n+1) < Array.length Sys.argv
+    then opt.offset <- int_of_string Sys.argv.(n+1)
+    else raise Option_format;
+    get_option (n+2) opt
+  | _ -> raise Option_format
+  else opt
+
 let main () =
   if Array.length Sys.argv < 2 then
-    print_endline "usage: zsim <input file>"
+    prerr_endline "Usage: zasm [input file] [options]"
   else
     try
       let input = open_in Sys.argv.(1) in
@@ -18,28 +39,29 @@ let main () =
       let asm = insts token lexbuf in
       let ofile = String.sub Sys.argv.(1) 0 (String.length Sys.argv.(1) - 2) in
       let output = open_out_bin ofile in
+      let opt = get_option 2 {vhdl = false; offset = 0} in
       let tbl = Hashtbl.create (List.length asm) in
       try
-        let asm' = align tbl 0 asm in
+        let asm' = align tbl (opt.offset * 2) asm in
         write output tbl asm';
-        if Array.length Sys.argv > 2 && Sys.argv.(2) = "-v"
-        (* then List.iter (show_vhdl stdout tbl) asm' *)
-        then show_vhdl stdout tbl 0 asm'
+        if opt.vhdl
+        then show_vhdl stdout tbl opt.offset asm'
         else List.iter (show stdout tbl) asm'
       with
-        | Unbound_label (l,p,m,a) -> print_endline "Unbound label:";
-          show_error stdout tbl (l,p,m,a)
-        | Duplicative_label (l,p,m,a) -> print_endline "Duplicative label:";
-          show_error stdout tbl (l,p,m,a)
-        | Unknown_instruction (l,p,m,a) -> print_endline "Unknown instruction:";
-          show_error stdout tbl (l,p,m,a)
-        | Immd_out_of_bounds (l,p,m,a) -> print_endline "Immediate out of bounds:";
-          show_error stdout tbl (l,p,m,a);
+        | Unbound_label (l,p,m,a) -> prerr_endline "Unbound label:";
+          show_error stderr tbl (l,p,m,a)
+        | Duplicative_label (l,p,m,a) -> prerr_endline "Duplicative label:";
+          show_error stderr tbl (l,p,m,a)
+        | Unknown_instruction (l,p,m,a) -> prerr_endline "Unknown instruction:";
+          show_error stderr tbl (l,p,m,a)
+        | Immd_out_of_bounds (l,p,m,a) -> prerr_endline "Immediate out of bounds:";
+          show_error stderr tbl (l,p,m,a);
       close_in input; close_out output
     with
-      | Sys_error e -> print_endline e
-      | Parsing.Parse_error -> print_endline "Parse Error"
-      | Failure s -> print_endline s
+      | Sys_error e -> prerr_endline e
+      | Parsing.Parse_error -> prerr_endline "Parse Error"
+      | Option_format -> print_options ()
+      | Failure s -> prerr_endline s
 
 ;;
 main ()
