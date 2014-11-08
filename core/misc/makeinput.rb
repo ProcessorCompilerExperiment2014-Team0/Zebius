b33f8be3d0495f09aa531af0a56746e4bc8ba2e4
Yuito Murase <yuito.murase@gmail.com>
2014-11-09 00:33:18 +0900

    improve makeinput.rb

--- core/misc/makeinput.rb
+++ core/misc/makeinput.rb
@@ -1,14 +1,23 @@
 #!/usr/bin/ruby2.0
 
+$zasm = "/home/zeptometer/Share/Zebius/asm/zasm"
+
+if (ARGV.length != 1 && ARGV.length != 2)
+  puts "usage: zasml asm (input)"
+  exit
+end
+
 code = ARGV[0]
 data = if (ARGV.length == 2)
          ARGV[1]
        else
          nil
        end
+bin = code[0...-2]
 
-size = File.size(code);
+`#$zasm #{code} -s 256`
+size = File.size(bin);
 
 File.open("tmp", "wb") {|f| f.write([size].pack("N"))}
-`cat tmp #{code} #{data || ""} > input.bin`
+`cat tmp #{bin} #{data || ""} > #{bin}.input`
 `rm tmp`
