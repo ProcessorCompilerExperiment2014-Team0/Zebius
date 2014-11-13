59102c2f08816e03532cc01b6f6eca2ecdacaffb
Yuito Murase <yuito.murase@gmail.com>
2014-11-13 19:52:00 +0900

    change wtime

--- core/cpu.vhd
+++ core/cpu.vhd
@@ -40,7 +40,9 @@ end cpu;
 
 architecture behavior of cpu is
 
-  constant wtime : unsigned(15 downto 0) := x"0d80";
+--  constant wtime : unsigned(15 downto 0) := x"0d80";
+  -- when 4800 and 33.33: 1ae8
+  constant wtime : unsigned(15 downto 0) := x"1ae8";
 
   signal ci  : core_in_t;
   signal co  : core_out_t;
